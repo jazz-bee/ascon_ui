4d39c36d490f4323fbc824796fa3e9832025a62f
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2023-12-05 14:19:04 -0300

    create AppWindow class: the customtkinter window

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-from tkinter import *
-# from customtkinter import CTk,CTkButton
 import customtkinter
-
-#Setear apariencia y colores
+# Setear apariencia y colores
 customtkinter.set_appearance_mode("System")  # Modos: system (default), light, dark
 customtkinter.set_default_color_theme("green")  # Temas: blue (default), dark-blue, green
 
+class AppWindow(customtkinter.CTk): # hereda de Ctk, AppWindow es una customtkinter window
+    def __init__(self):
+        super().__init__()
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+        self.title('App Criptografia: ASCON')
+        self.geometry("800x480")
 
-app.title('App Criptografia')
-app.geometry("400x240")
+        #Widgets
+        self.button=customtkinter.CTkButton(master=self, text="CTkButton", command=self.button_function)
+        self.button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
-#debug
-def button_function(): 
-    print("button pressed")
-
-# Boton
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
-
+    #methods
+    def button_function(self):
+        print("button pressed") #debug    
+            
+app = AppWindow()
 app.mainloop()

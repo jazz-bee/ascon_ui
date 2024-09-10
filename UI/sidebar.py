7822c69aa6f407dc5a703940f97138a6895538bd
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2024-09-10 17:09:01 -0300

    move gridconfig from Appwindow to widgets

--- UI/sidebar.py
+++ UI/sidebar.py
@@ -2,8 +2,12 @@ from customtkinter import CTkFrame, CTkLabel, CTkButton, CTkFont
 
 
 class SidebarFrame(CTkFrame):
-    def __init__(self, master, on_demo_click, on_aead_click, **kwargs):
+    def __init__(self, master, on_demo_click, on_aead_click, width=140, corner_radius=0, **kwargs):
         super().__init__(master, **kwargs)
+
+        # Set up the grid
+        # Span the sidebar across all rows (rowspan needs a large number e.g. 10)
+        self.grid(row=0, column=0, rowspan=10, sticky="nsew")
 
         # Store callback functions
         self.on_demo_click = on_demo_click
@@ -13,8 +17,13 @@ class SidebarFrame(CTkFrame):
             self, text="ASCON", font=CTkFont(size=30, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
+        self.grid_rowconfigure(1, minsize=20)   # Second row is for space
+
         self.demo_button = CTkButton(
-            self, text="Demo", command=self.on_demo_click).grid(row=1, column=0, padx=20, pady=10)
+            self, text="Demo", font=CTkFont(weight="bold"), command=self.on_demo_click).grid(row=2, column=0, padx=20, pady=10)
 
         self.aead_button = CTkButton(
-            self, text="AEAD", command=self.on_aead_click).grid(row=2, column=0, padx=20, pady=10)
+            self, text="AEAD", font=CTkFont(weight="bold"), command=self.on_aead_click).grid(row=3, column=0, padx=20, pady=10)
+
+        self.info_button = CTkButton(
+            self, text="About", font=CTkFont(weight="bold")).grid(row=4, column=0, padx=20, pady=10)
